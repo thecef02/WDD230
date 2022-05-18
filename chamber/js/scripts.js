c040775a1111ff44a9e6d81cb023499bb2ea3cd9
thecef02 <83976385+thecef02@users.noreply.github.com>
2022-05-18 12:48:41 -0600

    Lesson05 chamber

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -10,6 +10,13 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
+const todayIs = now.getDay();
+if (todayIs === 1 || todayIs === 2){
+    const banner = document.querySelector("#join-us-banner")
+    banner.style.display = "block"
+}
+
+
 
   
 
