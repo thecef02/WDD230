be867cbed1edf47423b71e560bb0f0c6764128c3
thecef02 <83976385+thecef02@users.noreply.github.com>
2022-05-18 11:57:18 -0600

    chamber

---
+++ chamber/js/scripts.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+const datefield = document.querySelector("aside");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+  
+
+
+
+
