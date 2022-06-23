b5aa32d46d4fcfc9dc9dfc65a385221f0bad05b5
thecef02 <83976385+thecef02@users.noreply.github.com>
2022-06-23 15:58:52 -0600

    Lesson10

---
+++ Lesson10/js/weatherapi.js
@@ -0,0 +1,43 @@
+
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Utah,US&appid=a1cdf4d637caf46a9288686067728afa&units=imperial"
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+        
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+
+  }
+
+function displayResults(weatherData){
+    console.log(weatherData); // this is for testing the call
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc.split(' ').map(w => w[0].toUpperCase() + w.substring(1).toLowerCase()).join(' ');;
+}
+
+apiFetch()
+
+
+
+
+
