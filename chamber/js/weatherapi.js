61e389dbc67cbe6c467013089c676263c0ec43db
thecef02 <83976385+thecef02@users.noreply.github.com>
2022-06-24 14:51:00 -0600

    Lesson10 2nd

---
+++ chamber/js/weatherapi.js
@@ -0,0 +1,70 @@
+
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#weather-desc');
+const windSpeed = document.querySelector('#speed');
+const cf = document.querySelector("#tempType");
+const btnCF = document.querySelector("#cf");
+btnCF.addEventListener("click",apiFetch);
+const url = "https://api.openweathermap.org/data/2.5/weather?q=american%20fork,UT,US&appid=a1cdf4d637caf46a9288686067728afa&units=imperial"
+let data = {}
+
+
+//  api.openweathermap.org/data/2.5/forecast/daily?lat=39.2502&lon=-111.751&cnt=7&appid=a1cdf4d637caf46a9288686067728afa&units=imperial
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        data = await response.json();
+        updatePage()
+        
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+
+  }
+function displayResults(weatherData){
+    
+    //currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc.split(' ').map(w => w[0].toUpperCase() + w.substring(1).toLowerCase()).join(' ');;
+
+    windSpeed.textContent = weatherData.wind.speed.toFixed(1);
+    
+}
+function setTempType(weatherData){
+  //weatherData.main.temp = weatherData.main.temp - 30; test to see if it works in case the temp > 50
+  if (cf.textContent == "°C" | cf.textContent == ""){
+    currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
+    cf.textContent = "°F"
+    displayWindChill(false);
+  } else{
+    currentTemp.innerHTML = `${fahrenheitToCelsius(weatherData.main.temp).toFixed(0)}`;
+    cf.textContent = "°C"
+    displayWindChill(true);
+  }
+}
+function fahrenheitToCelsius(fahrenheitTemp){
+  return (fahrenheitTemp - 32) * (5/9)
+}
+
+function updatePage(){
+ // console.log(data);
+  displayResults(data);
+  setTempType(data);
+}
+
+apiFetch();
+
+
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,12 +1,21 @@
-const temperature = document.getElementById('temp').innerHTML;
-const windspeed = document.getElementById('speed').innerHTML;
 
 
-let windchill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temperature * Math.pow(windspeed, .16));
-if (temperature <= 50 && windspeed > 3) {
-   windchill = Math.round(windchill);
-} else {
-   windchill = "n/a";
+
+function displayWindChill(tempInCelcius){
+   const temperature = data.main.temp; //document.getElementById('temp').innerHTML;
+   const windspeed = data.wind.speed //document.getElementById('speed').innerHTML;
+
+
+   let windchill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temperature * Math.pow(windspeed, .16));
+   if (temperature <= 50 && windspeed > 3) {
+      windchill = Math.round(windchill);
+   } else {
+      windchill = "n/a";
+      return
+   }
+   if (tempInCelcius ) {
+      document.getElementById('chill').innerHTML = fahrenheitToCelsius(windchill).toFixed(0) + " °C";
+   }else{
+      document.getElementById('chill').innerHTML = windchill + " °F";
+   }
 }
-document.getElementById('chill').innerHTML = windchill + " °F";
-
